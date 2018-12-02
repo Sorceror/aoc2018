bfab1b0cf651d897145e70fb1e400ce37f6a823d
Pavel Janecka <janecka.pavel@gmail.com>
2018-12-02 16:15:49 +0100

    Day 01 complete

---
+++ src/aoc2018/utils.clj
@@ -0,0 +1,7 @@
+(ns aoc2018.utils)
+
+(defn resource->strings [input-file]
+  (->>
+    (slurp (clojure.java.io/resource input-file))
+    (clojure.string/split-lines)
+    (map #(clojure.string/trim %))))
